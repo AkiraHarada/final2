c559e798dfe4fe95d6a9bc588516c22e44af67d7
AkiraHarada <Akira@haradaou-no-MacBook-Pro.local>
2016-02-23 18:45:58 +0900

    initial commit

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,9 +2,10 @@
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-    ofBackground(0, 0, 0);
+    
     ofSetWindowShape(640, 480);
     ofSetFrameRate(30);
+    //ofBackground(0, 0, 0);
     
     kinect.setup();
     kinect.setRegister(true);
@@ -12,46 +13,64 @@ void ofApp::setup() {
     kinect.addDepthGenerator();
     kinect.addImageGenerator();
     
+    kinect.addHandsGenerator();
+    kinect.addAllHandFocusGestures();
+    kinect.setMaxNumHands(2);
+    
     kinect.getDepthGenerator().GetAlternativeViewPointCap().SetViewPoint(kinect.getImageGenerator());
     
     kinect.start();
     
-    //maskedImage.allocate(640, 480, OF_IMAGE_COLOR_ALPHA);
-    //scenery.loadImage("stage.jpeg");
-    //circleImage.allocate(640, 480, OF_IMAGE_GRAYSCALE);
+    //mode = 0;
+    ballappear = false;
     
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
     kinect.update();
-    //unsigned char *maskedImageData = maskedImage.getPixels();
-    //unsigned char *imageData = kinect.getImagePixels().getPixels();
-    //unsigned char *circleImageData = circleImage.getPixels();
+    scenery.loadImage("stage.jpg");
+    
     unsigned short *depthData = kinect.getDepthRawPixels().getPixels();
     
+    for(int k = 0; k < 640*480; k++){
+        circleImageData[k] = 0;
+    }
+    for(int i = 0; i < 32*24; i++){
+        circleADD[i] = 0;
+    }
     
     for(int k = 0; k < 640*480; k++){
-        if(880 <= depthData[k] && depthData[k] < 1200){
+        if(880 <= depthData[k] && depthData[k] < 1300){
             circleImageData[k] = 1;
         } else{
             circleImageData[k] = 0;
         }
     }
-//    for(int i = 0; i < 640*480; i++){
-//        if((circleImageData[i*4+3]) == 0){
-//            circleRGBAsum[i]=0;
-//        } else{
-//            circleRGBAsum[i]=1;
-//        }
-//    }
-    for(int i=0; i < 12; i++){
-        for(int j=0; j < 16; j++){
-            for(int k=0; k < 40; k *=640){
-                for(int l=0; l < 40; l++){
-                    circleADD[j+16*i] += circleImageData[640*40*i+j*40+k+l];
+    for(int i=0; i < 24; i++){
+        for(int j=0; j < 32; j++){
+            for(int k=0; k < 20; k++){
+                for(int l=0; l < 20; l++){
+                    circleADD[j+32*i] += circleImageData[640*20*i+j*20+640*k+l];
                 }
             }
+        }
+    }
+    if(kinect.getNumTrackedHands() > 0){
+        for(int i = 0; i < kinect.getNumTrackedHands(); i++){
+            ofxOpenNIHand hand = kinect.getTrackedHand(i);
+            p[i] = hand.getPosition();
+            //ofCircle(p[i].x, p[i].y, 30);
+            int newacc = ofDist(p[i].x, p[i].y, preP[i].x, preP[i].y);
+            circleY[i] = ofNoise(50.0);
+            if (acc[i] < 20 && newacc > 20) {
+                ballappear = true; //一度現れたら消えない
+            }
+            acc[i] = newacc;
+        }
+        for(int j = 0; j < 2; j++){
+            preP[j] = p[j];
         }
     }
 }
@@ -59,12 +78,44 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    for(int i=0; i < 12; i++){
-        for(int j=0; j < 16; j++){
-            if(circleADD[j+16*i]>480){
-                ofSetColor(255, 255, 255);
-                ofCircle(20+(j-1)*40, 20+(i-1)*40, 20);
+    //    kinect.drawDepth(0,0,640,480);
+    ofDisableNormalizedTexCoords();
+    ofBackground(0, 0, 0);
+    ofSetColor(255, 255, 255);
+    scenery.draw(0, 0, 640, 480);
+    for(int i=0; i < 24; i++){
+        for(int j=0; j < 32; j++){
+            if(circleADD[j+32*i]>120){
+                ofMap(circleADD[j+32*i], 0, 400, 0, 360);
+                ofSetColor(ofRandom(255), ofRandom(255), ofRandom(255), 255*circleADD[j+32*i]/360);
+                
+            } else if((circleADD[j+32*i] <= 120) || (circleADD[j+32*i] > 90)){
+                ofMap(circleADD[j+32*i], 0, 400, 0, 360);
+                ofSetColor(ofRandom(255), ofRandom(255), ofRandom(255), 255*circleADD[j+32*i]/360);
+                
+            } else {
+                ofMap(circleADD[j+32*i], 0, 400, 0, 360);
+                ofSetColor(ofRandom(255), ofRandom(255), ofRandom(255), 255*circleADD[j+32*i]/360);
             }
+            ofTriangle(10+j*20, 10+i*20,
+                       5+j*20, 10+i*20,
+                       10+j*20, i*20);
+            ofTriangle(10+j*20, 10+i*20,
+                       15+j*20, 10+i*20,
+                       10+j*20, i*20);
+            ofTriangle(10+j*20, 10+i*20,
+                       5+j*20, 10+i*20,
+                       10+j*20, 20+i*20);
+            ofTriangle(10+j*20, 10+i*20,
+                       15+j*20, 10+i*20,
+                       10+j*20, 20+i*20);
+        }
+    }
+    for(int i = 0; i < 2; i++){
+        if(ballappear == true){
+            ofSetColor(0, 255, 255);
+            //ofCircle(p[i].x, 50+circleY[i], 30);
+            ofCircle(30+ofNoise(30.0, 60.0), p[i].y, 30);
         }
     }
 }
@@ -96,9 +147,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    //    unsigned short*depthData = kinect.getDepthRawPixels().getPixels();
-    //    unsigned short depthMM = depthData[y*640 + x];
-    //    sprintf(distanceBuffer, "%d mm", depthMM);
+    
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,28 +4,34 @@
 
 class ofApp : public ofBaseApp{
     
-//ofxOpenNI
+    //ofxOpenNI
 private:
     ofxOpenNI kinect;
-    ofImage maskedImage,scenery;
-    int circleRGBAsum[640*480], circleADD[16*12], circleImageData[640*480];
-    ofColor Circlecolor[16*12];
+    //ofImage maskedImage;
+    float circleADD[32*24];
+    int circleImageData[640*480];
+    //vector<int> circleImageData, circleADD;
     //char distanceBuffer[100];
 public:
     
-	void setup();
-	void update();
-	void draw();
+    void setup();
+    void update();
+    void draw();
     void exit();
-	void keyPressed  (int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y );
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
     
-    
-    
+    ofImage scenery;
+    ofPoint p[2];
+    ofPoint preP[2];
+    float acc[2];
+    int mode;
+    float  circleY[2];
+    bool ballappear;
 };
 
